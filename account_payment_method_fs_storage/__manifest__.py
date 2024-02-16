9538c70e282bf8e0ff085af24295801926654ae4
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2024-02-16 14:19:41 +0100

    [16.0][ADD] account_payment_method_fs_storage: init

---
+++ account_payment_method_fs_storage/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Payment Method Fs Storage",
+    "summary": """
+        Add the possibility to specify on the payment method,
+        a storage where files generated will be pushed to upon payment
+    """,
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "depends": [
+        "account_payment_order",
+        "fs_storage",
+    ],
+    "data": [
+        "views/account_payment_method.xml",
+    ],
+    "demo": [],
+}
---
+++ account_payment_method_fs_storage/readme/USAGE.rst
@@ -0,0 +1,1 @@
+When generating payment file on a payment order, the file will be pushed to a specific storage (set on the payment method).
