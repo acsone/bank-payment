d9d70bf46ea0c92727b4f6ea60fa1de0389fe051
Lindsay <lindsay.marion@acsone.eu>
2024-06-04 16:16:13 +0200

    [IMP] Allow to use the storage either from the payment mode or from the payment method

--- account_payment_method_or_mode_fs_storage/models/account_payment_method.py
+++ account_payment_method_or_mode_fs_storage/models/account_payment_method.py
@@ -1,6 +1,8 @@
 # Copyright 2024 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import ast
+
 from odoo import fields, models
 
 
@@ -18,8 +20,14 @@ class AccountPaymentMethod(models.Model):
         This is to avoid giving access to the model fs.storage
         at groups other than base.group_system
         """
-        storages = (
-            self.env["fs.storage"].sudo().search([("use_on_payment_method", "=", True)])
-        )
-
-        return [(str(r.id), r.display_name) for r in storages]
+        fs_storage_source = self.env.company.fs_storage_source_payment
+        if fs_storage_source == "method":
+            ids = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("account_payment_method_or_mode_fs_storage.fs_storage_ids")
+            )
+            if ids:
+                storages = self.env["fs.storage"].browse(ast.literal_eval(ids))
+                return [(str(r.id), r.display_name) for r in storages]
+        return []
---
+++ account_payment_method_or_mode_fs_storage/models/fs_storage.py
@@ -0,0 +1,9 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class FsStorage(models.Model):
+
+    _inherit = "fs.storage"
