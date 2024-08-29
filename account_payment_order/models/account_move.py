386628aaad2c21bd1d05688b05b7821f0835ca39
Lindsay <lindsay.marion@acsone.eu>
2024-08-29 16:05:04 +0200

    [16.0][IMP] Take allow_out_payment into account on payment order
        A previous MR existed but only when payment order was triggered from invoices.
        We added the case also directement from confirm button on payment order

--- account_payment_order/models/account_move.py
+++ account_payment_order/models/account_move.py
@@ -5,7 +5,6 @@
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-from odoo.fields import first
 
 
 class AccountMove(models.Model):
@@ -157,18 +156,7 @@ class AccountMove(models.Model):
             for line in applicable_lines.filtered(
                 lambda l: l.account_id.account_type == "liability_payable"
             ):
-                bank = line.partner_bank_id or first(line.partner_id.bank_ids)
-                if bank and not bank.allow_out_payment:
-                    raise UserError(
-                        _(
-                            'The option "Send Money" is not enabled on the bank '
-                            "account %(bank_account)s of partner %(partner)s."
-                        )
-                        % {
-                            "bank_account": bank.bank_name,
-                            "partner": line.partner_id.name,
-                        }
-                    )
+                line._check_bank_allows_out_payments()
 
             for payment_mode in payment_modes:
                 payorder = apoo.search(
