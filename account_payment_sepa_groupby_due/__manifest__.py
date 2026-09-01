ce79a4793eeaa6b1d97db7acdd9e0e20a39bd6e4
Tobias Zehntner <tobias.zehntner@acsone.eu>
2026-09-01 17:52:34 +0200

    account_payment_sepa_groupby_due wip

---
+++ account_payment_sepa_groupby_due/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Payment Sepa Groupby Due',
+    'summary': """Group SEPA payment orders by due date""",
+    'category': 'Banking',
+    'version': '18.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu',
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
